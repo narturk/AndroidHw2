41a2aed4101129529dd743b684bd62a978f43ba5
guyraz89 <gnewr89@gmail.com>
2019-05-16 22:55:21 +0300

    new design for questions

--- Trivia_Questions_Game/app/src/main/java/com/example/trivia_questions_game/Game_Activity.java
+++ Trivia_Questions_Game/app/src/main/java/com/example/trivia_questions_game/Game_Activity.java
@@ -3,6 +3,8 @@ package com.example.trivia_questions_game;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +12,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
-public class Game_Activity extends AppCompatActivity{
+public class Game_Activity extends AppCompatActivity {
+
+    private RelativeLayout relativeLayout;
+    private AnimationDrawable animationDrawable;
     private TextView textViewQuestion;
     private TextView textViewScore;
     private TextView textViewQuestionCount;
@@ -36,6 +42,18 @@ public class Game_Activity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // init constraintLayout
+        relativeLayout = (RelativeLayout) findViewById(R.id.rel_layout);
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(5000);
+
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(2000);
 
         textViewQuestion = findViewById(R.id.text_view_question);
         textViewScore = findViewById(R.id.text_view_score);
@@ -103,5 +121,24 @@ public class Game_Activity extends AppCompatActivity{
                     }
                 })
                 .show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
     }
 }
